5b90152d49aaf724483294507aea2e1825026545
Smikey <smikeywork@gmail.com>
2022-07-05 17:39:43 +0545

    :tada: Begin with string concatination

---
+++ basic/string.py
@@ -0,0 +1,40 @@
+
+# This is \\ double backslash
+# This is /\/\/\/\/\ mountain
+# he is     happy.
+# \" \n \t \'
+
+print(r"This is \\ double backslash")
+print(r"This is /\/\/\/\/\ mountain")
+
+# # This is \\\\ double backslash
+# This is /\\/\\/\\/\\/\\ mountain
+# he is\thappy.
+# " \\\" \\n \\t \\\' "
+
+# print(9-3)
+# print(9/3)  #fps
+# print(9//3)  #int division
+# print(9*3)  #multiplication
+# print(2**3)  #power
+
+print(round(2**3.14,3))  #power
+
+# print((2**3)/(4-2)*6+6)
+print(5%2) # modulo
+'''
+Divider-->2
+Divident --> 5
+Remiender -->1
+output = 1
+'''
+
+# Emoji
+# print("U+1f647")   # replace + with three zeros 000
+print("\U0001f647")
+
+
+# String Concatination and int to string conversion
+# print("Aryama"+str(10))
+# print(10+int("as"))
+
